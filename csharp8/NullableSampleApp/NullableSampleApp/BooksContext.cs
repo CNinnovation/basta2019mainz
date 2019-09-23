37efb60d9f889f56c5496d59cfd8eb6517a37741
Christian Nagel <christian@christiannagel.com>
2019-09-23 23:44:29 +0200

    nullable sample

---
+++ csharp8/NullableSampleApp/NullableSampleApp/BooksContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NullableSampleApp
+{
+#nullable disable
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+#nullable restore
+}
---
+++ csharp8/NullableSampleApp/NullableSampleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+#nullable enable
+
+namespace NullableSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            book.Title = "Professional C#";
+        }
+
+        static void ShowBook(Book book)
+        {
+            Console.WriteLine($"{book.Title} {book.Publisher.ToUpper()}");
+        }
+    }
+}
