dabfe8912ad76631669e3360aa5b1fe42fd39dc4
Christian Nagel <christian@christiannagel.com>
2019-09-24 09:23:46 +0200

    cleanup ranges

--- csharp8/RangesSample/RangesSample/ListExtensions.cs
+++ csharp8/RangesSample/RangesSample/ListExtensions.cs
@@ -6,13 +6,19 @@ namespace RangesSample
 {
     public static class ListExtensions
     {
-        public static IList<T> Range<T>(this IList<T> list, Range range) 
-        { 
-            var count = list.Count; 
-            var start = range.Start.IsFromEnd ? count - range.Start.Value : range.Start.Value; 
-            var end = range.End.IsFromEnd ? count - range.End.Value : range.End.Value; 
-            if (start > end) (start, end) = (end, start); 
-            var rng = Enumerable.Range(start, end - start); 
-            return list.Where((item, index) => rng.Any(x => x == index)).ToList(); }
+        public static IList<T> Range<T>(this IList<T> list, Range range)
+        {
+            var count = list.Count;
+            var start = range.Start.IsFromEnd ? count - range.Start.Value : range.Start.Value;
+            var end = range.End.IsFromEnd ? count - range.End.Value : range.End.Value;
+            if (start > end) (start, end) = (end, start);
+            var rng = Enumerable.Range(start, end - start);
+            return list.Where((item, index) => rng.Any(x => x == index)).ToList();
+        }
+
+        //public static IList<T> Slice<T>(this IList<T> list, int start, int length)
+        //{
+        //    return list.Skip(start).Take(length).ToList();
+        //}
     }
 }
--- csharp8/RangesSample/RangesSample/Program.cs
+++ csharp8/RangesSample/RangesSample/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RangesSample
 {
@@ -6,10 +8,12 @@ namespace RangesSample
     {
         static void Main(string[] args)
         {
+            Range r;
             byte[] buffer = new byte[4096];
             Span<byte> span = buffer.AsSpan();
             var slice = span.Slice(0, 50);
             slice.Fill(42);
+
 
             // use range instead of slice           
             var slice2 = span[0..100];
@@ -24,19 +28,22 @@ namespace RangesSample
             string brownfoxjumped = fox1[10..];
             string thequick = fox1[..9];
             string fox2 = fox1[..];
+           
 
             var range = 4..9;
             var index = ^3;
 
-
-            // write character by character
-            foreach (var item in fox1[^4..])
+            string[] names = { "James", "Niki", "Jochen", "Juan", "Michael", "Sebastian", "Nino", "Lewis" };
+            foreach (var name in names[2..^2])
             {
-                Console.Write(item);
+                Console.WriteLine(name);
             }
+
+            var coll = new MyCollection();
+            var slice1 = coll[20..^55];
+
+
             Console.WriteLine();
-
-
         }
     }
 }
