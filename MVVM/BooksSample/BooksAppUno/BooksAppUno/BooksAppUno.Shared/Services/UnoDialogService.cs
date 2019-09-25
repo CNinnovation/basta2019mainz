60c45091d608b921663a30db3bbae245ad5564d0
Christian Nagel <christian@christiannagel.com>
2019-09-25 10:17:13 +0200

    platform uno

---
+++ MVVM/BooksSample/BooksAppUno/BooksAppUno/BooksAppUno.Shared/Services/UnoDialogService.cs
@@ -0,0 +1,25 @@
+﻿using GenericViewModels.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace BooksAppUno.Shared.Services
+{
+    public class UnoDialogService : IDialogService
+    {
+        public async Task ShowAsync(string title, Exception ex, string ok = "Ok")
+        {
+            await ShowAsync(title, ex.Message, ok);
+        }
+
+        public async Task<bool> ShowAsync(string title, string content, string ok = "Ok", string cancel = null)
+        {
+            var dialog = new MessageDialog(content, title);
+            await dialog.ShowAsync();
+            return true;
+        }
+    }
+}
